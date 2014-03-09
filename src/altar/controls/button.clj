653dfd7c1b9bcc1c63c5ddaeccb74d9e82b0b391
Adam Avramov <fallenblood@gmail.com>
2014-03-09 17:13:01 +0200

    Handle state using atom.

--- src/altar/controls/button.clj
+++ src/altar/controls/button.clj
@@ -3,14 +3,50 @@
   (:require [clojure.data :refer [diff]]))
 
 
-; MIDI IO
+; Momentary button function
+
+(defn momentary
+  [input turn-on turn-off]
+  {:pre [(not (contains? input :command))]}  ; command is implicit
+  (fn button-handler [msg]
+    (when (nil? (first (diff (conj input {:command :note-on}) msg))) (turn-on msg))
+    (when (nil? (first (diff (conj input {:command :note-off}) msg))) (turn-off msg))
+    (momentary input turn-on turn-off)))
+
+(def toggle-state {:on :off
+                   :off :on})
+
+(defn toggle
+  [state input turn-on turn-off]
+  {:pre [(not (contains? input :command))]}  ; command is implicit
+  (fn button-handler [msg]
+    (when (nil? (first (diff (conj input {:command :note-on}) msg))))
+    (when (nil? (first (diff (conj input {:command :note-off}) msg))))
+    (toggle (toggle-state :off) input turn-on turn-off)))
+
+
+; MIDI event handler
+
+(defn midi-handler-recur
+  [controls]
+  (fn [midi-msg]
+    (loop [controls (deref controls)]
+      (when-not (empty? controls)
+        ((first controls) midi-msg)  ; call topmost control fn with midi msg
+        (recur (rest controls))))))
+
+(defn midi-handler-flat
+  [control-atom]
+  (fn [midi-msg]
+    (let [updated-control-atom (doall (map #(% midi-msg) (deref control-atom)))]
+      (reset! control-atom updated-control-atom))))
+
+
+; Controller-specific stuff
 
 (def mm1-in (midi/midi-in "MM-1"))
 
 (def mm1-out (midi/midi-out "MM-1"))
-
-
-; Controller-specific stuff
 
 (defn mm1-off [msg]
   (println "off" (:data1 msg))
@@ -27,59 +63,22 @@
   (midi/midi-note-on mm1-out (:data1 msg) 2 4)
   msg)
 
-
-; Momentary button function
-
-(defn momentary
-  [input handler-on handler-off]
-  {:pre [(not (contains? input :command))]}  ; command is implicit
-  (fn button-handler [msg]
-    (println "momentary")
-    (when (nil? (first (diff (conj input {:command :note-on}) msg)))
-      (handler-on msg))
-    (when (nil? (first (diff (conj input {:command :note-off}) msg)))
-      (handler-off msg))
-    (msg)))
-
-(defn toggle
-  [input handler-on handler-off]
-  {:pre [(not (contains? input :command))]}  ; command is implicit
-  (fn button-handler [msg]
-    (println "toggle")
-    (when (nil? (first (diff (conj input {:command :note-on}) msg)))
-      (handler-on msg))
-    (when (nil? (first (diff (conj input {:command :note-off}) msg)))
-      (handler-off msg))
-    (msg)))
-
-
-; MIDI event handler
-
-; (defn midi-handlers
-;   [& controls]
-;   (fn [midi-msg]
-;     (loop [controls controls]
-;       (when-not (empty? controls)
-;         ((first controls) midi-msg)  ; call topmost control fn with midi msg
-;         (recur (rest controls))))))
-
-(defn midi-handlers
-  [& controls]
-  (fn [midi-msg]
-    (doall (map #(% midi-msg) controls))))
+(defn mm1-toggle-press [state msg])
 
 
 ; Connect handler and controls to MIDI event
 
+
+(def controls* (atom [(momentary {:channel 4 :data1 19} mm1-on mm1-off)
+                      (momentary {:channel 4 :data1 20} mm1-on mm1-off)
+                      (momentary {:channel 4 :data1 23} mm1-on mm1-off)
+                      (momentary {:channel 4 :data1 24} mm1-on mm1-off)
+                      (toggle :off {:channel 4 :data1 27} mm1-on mm1-off)
+                      (toggle :off {:channel 4 :data1 28} mm1-on mm1-off)
+                      (toggle :off {:channel 4 :data1 31} mm1-on mm1-off)
+                      (toggle :off {:channel 4 :data1 32} mm1-on mm1-off)]))
+
 (def receiver
   (midi/midi-handle-events
     mm1-in
-    (midi-handlers
-      (momentary {:channel 4 :data1 19} mm1-on mm1-off)
-      (momentary {:channel 4 :data1 20} mm1-on mm1-off)
-      (momentary {:channel 4 :data1 23} mm1-on mm1-off)
-      (momentary {:channel 4 :data1 24} mm1-on mm1-off)
-      (momentary {:channel 4 :data1 27} mm1-on mm1-off)
-      (momentary {:channel 4 :data1 28} mm1-on mm1-off)
-      (momentary {:channel 4 :data1 31} mm1-on mm1-off)
-      (momentary {:channel 4 :data1 32} mm1-on mm1-off))))
+    (midi-handler-flat controls*)))
